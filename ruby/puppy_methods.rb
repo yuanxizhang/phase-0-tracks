91be890456f33ea48805a7af2b5c3f8a8aaa7528
yuanxizhang <yuanxi.zhang@gmail.com>
2016-11-13 19:20:56 -0800

    Update puppy_methods.rb with new features

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,72 @@
+# Declare class Puppy and class Vehicle 
 class Puppy
-
+  
+  def initialize
+  	puts "initializing new puppy instance..."
+  end
+  
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(number)
+  	puts "Woof! "*number
+  end
+  
+  def roll_over
+  	puts "*Rolls over*"
+  end
+  
+  def dog_years(years)
+  	p years / 7
+  end
+  
+  def eat(food)
+  	puts "I like to eat #{food}"
+  end
 end
+
+class Vehicle
+	def initialize(color,make,year)
+		@color = color
+		@make = make
+		@year = year
+		@speed = 0
+	end
+	
+	def accelerate(speed)
+		@speed += speed
+		puts "Your #{@color} #{@make} is going #{@speed} mph"
+	end
+	
+	def stop
+		@speed = 0
+	end
+	
+	def car_info
+		puts "Your #{@color} #{@make} is made in #{@year}"
+		puts "******************************"
+	end
+end
+
+# Driver code for class Puppy
+winston = Puppy.new
+winston.fetch("ball")
+winston.speak(5)
+winston.roll_over
+winston.dog_years(77)
+winston.eat("Doggy biscuits")
+
+#Driver code for class Vehicle
+arr_car = []
+for year in 1950..2001
+	arr_car.push(Vehicle.new("red","honda civic", year) )
+end
+
+arr_car.each do |car|
+	car.accelerate(25)
+	car.car_info
+end
+
 
