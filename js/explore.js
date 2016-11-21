51a090fd42240e2099d30704e578d3039f74bae9
yuanxizhang <yuanxi.zhang@gmail.com>
2016-11-20 18:24:09 -0800

    Update explore.js with a new condition statement.

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,13 @@
 // step 1: split the string into an array of individual letters, 
 // steo 2: reverse the order of the letter array
 // step 3: join the new letter array into a new string, return the new string
+
 function reverse(str){
-	return str.split('').reverse().join('');
+	newString = str.split('').reverse().join('');
+	return newString;
 };
 
-console.log(reverse("potato"));
+var myPotato = reverse("potato");
+if (3==3){
+	console.log(myPotato);
+};
